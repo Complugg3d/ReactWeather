e4c1c4de761e9d6c9fd644e3031e86544ca479f6
Erick Garcia <erick.m.garcia.vega@gmail.com>
2017-05-20 14:39:47 -0500

    added extra configs to heroku

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ var port = process.env.PORT | 3000;
 //Create our app;
 var app = express();
 
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 app.use(express.static('public'));
+
+
 
 app.listen(port, () => {
   console.log(`Express server is running in the port ${port}`);
