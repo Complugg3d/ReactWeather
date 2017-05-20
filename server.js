6fda676ab09b4335568e72383aae5aba339866cc
Erick Garcia <erick.m.garcia.vega@gmail.com>
2017-05-20 14:35:50 -0500

    added heroku deploy configs

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
+var port = process.env.PORT | 3000;
 //Create our app;
 var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log('Express server is running in the port 3000');
+app.listen(port, () => {
+  console.log(`Express server is running in the port ${port}`);
 });
