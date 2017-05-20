527edb087514907f82036ab6af9e76bb00201a0c
Erick Garcia <erick.m.garcia.vega@gmail.com>
2017-05-20 14:05:23 -0500

    added wraper to examples

--- app/components/examples.js
+++ app/components/examples.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
 var Examples = (props) => {
-  return (<h3>Examples component</h3>);
+  return (<div>
+    <h3>Examples component</h3>
+    <p>Welcome to examples page</p>
+  </div>);
 }
 
 export default Examples;
